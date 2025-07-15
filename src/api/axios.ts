6fc53ed3c0497b8ef431ccf7845dfe3d320a8220
Cyrus-M2 <devsben@345@gmail.com>
2025-07-15 06:27:01 +0300

    updated axios with the live link for backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://blogit-backend-1-impj.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
@@ -11,5 +12,6 @@ instance.interceptors.request.use((config) => {
 });
 
 export default instance;
+
 
 
